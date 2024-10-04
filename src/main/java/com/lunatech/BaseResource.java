3c055f0bd130412f6d385649576a428d4d169052
Willem Jan Glerum <willem@wjglerum.nl>
2024-10-04 09:44:20 +0200

    Update Quarkus

--- src/main/java/com/lunatech/BaseResource.java
+++ src/main/java/com/lunatech/BaseResource.java
@@ -1,12 +1,11 @@
 package com.lunatech;
 
-import javax.annotation.security.RolesAllowed;
-import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.annotation.security.RolesAllowed;
 import org.eclipse.microprofile.jwt.JsonWebToken;
 
 import io.quarkus.oidc.AccessTokenCredential;
